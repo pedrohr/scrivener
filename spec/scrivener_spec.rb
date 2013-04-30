b3797eacbdb7dcd462e20a1cb17099a970e34b07
Pedro Assis <pedroh2306@gmail.com>
2013-04-29 23:51:19 -0300

    Parsing sentences and cleaning uninteresting structured text

--- spec/scrivener_spec.rb
+++ spec/scrivener_spec.rb
@@ -1,14 +1,70 @@
+# -*- coding: utf-8 -*-
+require 'nokogiri'
 require 'test/unit'
 require './src/scrivener.rb'
 
 class ScrivenerTest < Test::Unit::TestCase
   def setup
     @wikipedia_dump = './spec/mock/wikipedia_dump_sample.xml'
+    @invalid_xml_file = './spec/mock/invalid_xml_file.xml'
+
+    @example_text = "==Etymology and terminology==
+      {{Redirect|Anarchist|the fictional character|Anarchist (comics)}}
+      {{Redirect|Anarchists}}
+      {{pp-move-indef}}{{Use dmy dates|date=July 2012}}
+      {{Anarchism sidebar}}
+
+      [[File:WilliamGodwin.jpg|left|thumb|[[William Godwin]], &quot;the first to formulate the political and economical conceptions of anarchism, even though he did not give that name to the ideas developed in his work&quot;.&lt;ref name=&quot;EB1910&quot; /&gt;]]
+      &lt;!--Anarcho-communist Joseph Déjacque, the first person to use the term &quot;libertarian&quot; in a political sense and self-proclaimed advocate of libertarianism, needs to be added here. His work and stances on anarchism are very relevant to this particular section of the article. Additionally, his criticisms of Pierre-Joseph Proudhon's mutualism are very relevant here.--&gt;
+      Honestly, [[William Godwin]] was a crazy man.
+      * A silly link
+"
+
     @scrivener = Scrivener.new(@wikipedia_dump)
   end
 
   def test_should_open_wikimedia_dump_file
     file = File.open(@wikipedia_dump, 'r')
-    assert(File.identical?(@scrivener.wikipedia_dump_file, file))
+    assert(File.identical?(@scrivener.raw_wikipedia_dump_file, file))
+  end
+
+  # I assume that the XML is correctly parsed by the Nokogiri library if no exception
+  # is raised. Therefore, no unit test for this feature will be inserted here.
+
+  def test_should_parser_valid_xml_file
+    assert_nothing_raised do
+      @scrivener = Scrivener.new(@wikipedia_dump)
+    end
+
+    assert_raise Nokogiri::XML::SyntaxError do
+      Scrivener.new(@invalid_xml_file)
+    end
+  end
+
+  def test_should_parse_raw_file
+    assert_equal(@scrivener.processed_wikipedia_dump.nil?, false)
+  end
+
+  def test_should_detect_articles
+    @scrivener.parse_pages
+
+    assert_equal(@scrivener.articles.size, 2)
+
+    assert_equal(@scrivener.articles.first, ['AccessibleComputing', '#REDIRECT [[Computer accessibility]] {{R from CamelCase}}'])
+  end
+
+  def test_should_break_text_into_lines_and_resolve_links
+    @scrivener.parse_pages
+    assert_equal(@scrivener.parse_text(@example_text), 
+                 ["<http://dbpedia.org/resource/William_Godwin>, the first to formulate the political and economical conceptions of anarchism, even though he did not give that name to the ideas developed in his work.",
+                  "Anarcho-communist Joseph Déjacque, the first person to use the term libertarian in a political sense and self-proclaimed advocate of libertarianism, needs to be added here.",
+                  "His work and stances on anarchism are very relevant to this particular section of the article.",
+                  "Additionally, his criticisms of Pierre-Joseph Proudhon's mutualism are very relevant here.",
+                 "Honestly, <http://dbpedia.org/resource/William_Godwin> was a crazy man.",
+                 "A silly link."])
+  end
+
+  def test_should_replace_title_names_by_links
+    
   end
 end
--- src/scrivener.rb
+++ src/scrivener.rb
@@ -1,7 +1,116 @@
+require 'nokogiri'
+require 'punkt-segmenter'
+
 class Scrivener
-  attr_reader :wikipedia_dump_file
+  attr_reader :raw_wikipedia_dump_file, :processed_wikipedia_dump, :articles
 
   def initialize(filename)
-    @wikipedia_dump_file = File.open(filename, 'r')
+    @raw_wikipedia_dump_file = File.open(filename, 'r')
+
+    unless @raw_wikipedia_dump_file.nil?
+
+      print "\rLoading XML file..."
+      @processed_wikipedia_dump = Nokogiri::XML(@raw_wikipedia_dump_file.read) do |config|
+        config.strict
+      end
+    end
+  end
+
+  def parse_pages
+    pages = @processed_wikipedia_dump.search("page")
+
+    @articles = []
+    pages.each do |page|
+      @articles.push([page.search("title").children.first.to_s, page.search("text").children.first.to_s] ) unless page.search("text").nil?
+    end
+
+    @processed_wikipedia_dump = nil
+  end
+
+  def clear_inside_brackets(text)
+    text = text[2..text.size-3]
+
+    # check if its a File inside a bracket
+    if text.include?("File:")
+      params = text.split(",")
+      params[0] = "<http://dbpedia@@@org/resource/" + params[0].match(/\[\[(.+)\]\]/)[1].gsub(" ", "_") + ">"
+      text = params.join(",")
+    else
+      text = "<http://dbpedia@@@org/resource/" + text.gsub(' ', '_') + ">"
+    end
+
+    # replacing . for @@@ in links in order to avoid future misspliting in sentences
+
+    return text
+  end
+
+  def clean_text(text)
+    text.gsub!(/\[\[(.+)\]\]/) {|block| clear_inside_brackets(block)}
+
+    [/\&lt\;!--(.+)--\&gt\;/].each do |pt|
+      text.gsub!(pt, '\1')
+    end
+
+    [/\&lt\;ref(.+)\&gt\;/, "&quot;", /\{\{(.+)\}\}/, /==(.+)==/, "*"].each do |pt|
+      text.gsub!(pt, "")
+    end
+
+    [/\&lt\;(.+)\&gt\;/].each do |pt|
+      text.gsub!(pt, '\1')
+    end
+  end
+
+  def parse_text(text)
+    sentences = []
+
+    # resolve links and remove quotes and references
+    clean_text(text)
+
+    text.each_line do |paragraph|
+      prospective = paragraph.split(".")
+      
+      open = 0
+      aggregator = ""
+
+      prospective.each do |sentence|
+        sentence.strip!
+        aggregated = false
+
+        left = sentence.include?("[[")
+        right = sentence.include?("]]")
+
+        if left
+          open += 1
+
+          aggregator += sentence + "."
+          aggregated = true
+        end
+
+        if right
+          open -= 1
+
+          unless aggregated
+            aggregator += (sentence + ".")
+          end
+
+          if open == 0
+            if aggregator.split(" ").size > 1
+              aggregator.gsub!('@@@', '.')
+              sentences.push(aggregator)
+            end
+            aggregator = ""
+          end
+        end
+
+        unless left and right
+          if sentence.split(" ").size > 1
+            sentence.gsub!('@@@', '.')
+            sentences.push(sentence + ".")
+          end
+        end
+      end
+    end
+
+    return sentences
   end
 end
